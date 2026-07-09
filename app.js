c600628637dd011ec61cf562801b0e5bc310cb8e
Jonathan <40722881+jonlarson22@users.noreply.github.com>
2026-07-08 20:19:36 -0600

    Remove bot turn functions and clean up code

--- app.js
+++ app.js
@@ -913,13 +913,3 @@ function calculateHandScore(hand) {
         return total + parseInt(card.value, 10);
     }, 0);
 }
-
-function startBotTurn() {
-  isPlayerTurn = false;
-  document.getElementById('game-board').classList.add('board-locked');
-  }
-
-function endBotTurn() {
-  isPlayerTurn = true;
-  document.getElementById('game-board').classList.remove('board-locked');
-}
